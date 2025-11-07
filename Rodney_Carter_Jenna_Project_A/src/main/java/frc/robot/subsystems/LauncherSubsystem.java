38efb3ae1644370fbe440dc647b707bf4db72a9c
Smols <carterd@vandenrobotics.com>
2025-11-06 16:42:59 -0800

    Completed Launcher States
    Finished the switch case and all the methods needed for it

---
+++ Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -0,0 +1,64 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.security.Key;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LauncherSubsystem extends SubsystemBase {
+  /** Creates a new LauncherNeutralSubsystem. */
+  private TalonFX TopMotor;
+
+  //temporary string
+  public String launcherString = "69";
+
+public enum LauncherState {
+  S_neutral, S_accelerate
+  //creating states ^
+}
+  public static LauncherState mLauncherstate;
+
+  public LauncherSubsystem() {
+    //creating object ^
+    TopMotor = new TalonFX(0);
+    //Identifing object ^
+    mLauncherstate = LauncherState.S_neutral;
+    //Setting state ^
+  }
+
+  public void neutral() {
+    TopMotor.setVoltage(3);
+    //state action for neutral
+  }
+
+  public void accelerate() {
+    TopMotor.setVoltage(8);
+    //state action for accelerate
+  }
+
+  public void RunLauncherState() {
+    switch (mLauncherstate) {
+      case S_neutral:
+        neutral();
+        break;
+      case S_accelerate:
+        accelerate();
+        break;
+        // ^ making it so based on state it changes action 
+    }
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    RunLauncherState();
+    SmartDashboard.putString("LauncherState", launcherString);
+    //making so state gets changed ^
+  }
+}
