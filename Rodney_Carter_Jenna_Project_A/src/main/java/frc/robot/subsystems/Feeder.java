73e2e2bb5ece5092da0e481ca77296bacdfeb92c
JennaF <jennaf@vandenrobotics.com>
2025-11-17 16:49:55 -0800

    State Machine

--- Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/Feeder.java
+++ Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/Feeder.java
@@ -5,19 +5,52 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.fasterxml.jackson.databind.introspect.TypeResolutionContext.Empty;
 
 import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Feeder extends SubsystemBase {
-  private TalonFX Feedermotor; 
+  private TalonFX mFeedermotor; 
   /** Creates a new Feeder. */
  
+  public static FeederState mFeederState; 
+
+  public enum FeederState {
+
+  S_empty, S_full, S_feeding       
+    }
+   
   public Feeder() {
+mFeederState = FeederState.S_empty; 
 
-    Feedermotor = new TalonFX(0);  
+mFeedermotor = new TalonFX(0);
+  }
+public void runFeederState(){
+  switch (mFeederState) {
+
+      case S_empty:
+      spinFeederMotor();
+      break;
+     
+      case S_full:
+      stopFeedermotor();
+      break;
+     
+      case S_feeding:
+    FeedmotorFeed();
+      break;
+  }
 }
-
+  public void spinFeederMotor(){
+    mFeedermotor.setVoltage(4.1);
+  }
+public void stopFeedermotor(){
+  mFeedermotor.setVoltage(0);
+}
+public void FeedmotorFeed(){
+  mFeedermotor.setVoltage(6.7);
+}
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
