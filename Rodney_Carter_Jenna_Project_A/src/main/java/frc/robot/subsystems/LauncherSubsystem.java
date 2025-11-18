d3c80019400a86fbbad5f30dfdaf1eaebfd12c28
Smols <carterd@vandenrobotics.com>
2025-11-17 16:46:17 -0800

    Robot Container Buttons Completed
    With a lot of help I we were able to get the buttons working.

--- Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
+++ Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/LauncherSubsystem.java
@@ -9,11 +9,14 @@ import java.security.Key;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LauncherSubsystem extends SubsystemBase {
   /** Creates a new LauncherNeutralSubsystem. */
   private TalonFX TopMotor;
+  private TalonFX BottomMotor;
+  //creating motors
 
   //temporary string
   public String launcherString = "69";
@@ -34,11 +37,13 @@ public enum LauncherState {
 
   public void neutral() {
     TopMotor.setVoltage(3);
+    BottomMotor.setVoltage(3);
     //state action for neutral
   }
 
   public void accelerate() {
     TopMotor.setVoltage(8);
+    BottomMotor.setVoltage(8);
     //state action for accelerate
   }
 
