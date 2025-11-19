a6437f486c0d71d07a33647de6cc0d901f3a62dc
RodneyStewart III <rodneys@vandenrobotics.com>
2025-11-18 16:26:16 -0800

    Adjusted Formatting
    Added additional indenting to clean the formatting

--- Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/Feeder.java
+++ Rodney_Carter_Jenna_Project_A/src/main/java/frc/robot/subsystems/Feeder.java
@@ -18,16 +18,17 @@ public class Feeder extends SubsystemBase {
 
   public enum FeederState {
 
-  S_empty, S_full, S_feeding       
-    }
+    S_empty, S_full, S_feeding       
+  }
    
   public Feeder() {
-mFeederState = FeederState.S_empty; 
+    mFeederState = FeederState.S_empty; 
 
-mFeedermotor = new TalonFX(0);
+    mFeedermotor = new TalonFX(0);
   }
-public void runFeederState(){
-  switch (mFeederState) {
+
+  public void runFeederState(){
+    switch (mFeederState) {
 
       case S_empty:
       spinFeederMotor();
@@ -38,22 +39,21 @@ public void runFeederState(){
       break;
      
       case S_feeding:
-    FeedmotorFeed();
+      StartFeeding();
       break;
+    }
   }
-}
   public void spinFeederMotor(){
     mFeedermotor.setVoltage(4.1);
   }
-public void stopFeedermotor(){
-  mFeedermotor.setVoltage(0);
-}
-public void FeedmotorFeed(){
-  mFeedermotor.setVoltage(6.7);
-}
+  public void stopFeedermotor(){
+    mFeedermotor.setVoltage(0);
+  }
+  public void StartFeeding(){
+    mFeedermotor.setVoltage(6.7);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
-} 
+}
